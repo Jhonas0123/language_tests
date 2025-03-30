35f7394497d23fc5a247e8c507dc4ec0976028d3
Jhonatan Araujo <jhona01230123@gmail.com>
2025-03-30 16:04:25 -0400

    2 ejercicios

---
+++ Python/ejercicios/ejercicio_13.py
@@ -0,0 +1,15 @@
+""" condicion repetitiva con while """
+""" condicion while de orden ascendente desde el 0 al 5 """
+maximo = 5
+contador = 0
+while contador <= maximo:
+    print(f"{contador}")
+    contador = contador + 1
+
+print("descendente")
+
+while maximo >= contador:
+    print(f"{maximo}")
+    maximo = maximo - 1
+    
+        
